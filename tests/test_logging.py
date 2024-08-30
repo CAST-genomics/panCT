19570c08710bc3461936d55c2e46a4d30f45fcef
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2024-08-30 20:04:03 +0000

    fix failing log tests

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -25,7 +25,7 @@ def test_getLogger_instance():
 def test_getLogger_with_name():
     logger_name = "test_logger"
     logger = getLogger(name=logger_name)
-    assert logger.name == "haptools.test_logger"
+    assert logger.name == "panct.test_logger"
 
 
 def test_getLogger_with_level():
@@ -45,12 +45,12 @@ def test_getLogger_with_index_name():
     log = getLogger(name="index")
     log.setLevel(logging.ERROR)
     assert isinstance(log, logging.Logger)
-    assert log.name == "haptools.index"
+    assert log.name == "panct.index"
 
 
 def test_getLogger_with_default_parameters():
     logger = getLogger()
     assert isinstance(logger, logging.Logger)
-    assert logger.name == "haptools"
+    assert logger.name == "panct"
     # by default, the log level should be "ERROR"
     assert logger.level == logging.ERROR
