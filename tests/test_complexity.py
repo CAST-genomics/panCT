3452f9d4a267c6fab31908df40a2c5968ff5dbcb
serieva <109799187+serieva@users.noreply.github.com>
2024-09-12 13:05:22 -0400

    Update test_complexity.py
    Added test_complexity function to test short test files as inputs

--- tests/test_complexity.py
+++ tests/test_complexity.py
@@ -1,8 +1,15 @@
 import os
 from pathlib import Path
+from panct.complexity import complexity_score
 
 DATADIR = Path(__file__).parent.joinpath("data")
 
+def test_complexity():
+    gfa_short_test_file = DATADIR / "short_test.gfa"
+    tsv_short_test_file = DATADIR / "short_test_node_map.tsv"
+
+    complexity = complexity_score(str(gfa_short_test_file), str(tsv_short_test_file))
+    assert complexity == 0.875, f"Expected complexity score = 0.875, but got {complexity}"
 
 def test_basic():
     assert True
