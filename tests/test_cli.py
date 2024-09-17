f81b2043dd5dfecbd5e422b03d04774b1202c588
serieva <109799187+serieva@users.noreply.github.com>
2024-09-16 21:21:25 -0400

    Update test_cli.py
    Fixed the path of short test input files.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,15 @@
 from typer.testing import CliRunner
 from panct.__main__ import app
+from pathlib import Path
 
+DATADIR = Path(__file__).parent.joinpath("data")
 runner = CliRunner()
 
 def test_complexity_cli():
-    short_gfa_test_file = "tests/data/short_test.gfa"
-    short_tsv_test_file = "tests/data/short_test_node_map.tsv"
-    output_file = "tests/data/test_output.txt"
-
-    result = runner.invoke(app, ["complexity", short_gfa_test_file, short_tsv_test_file])
+    short_gfa_test_file = DATADIR / "short_test.gfa"
+    short_tsv_test_file = DATADIR / "short_test_node_map.tsv"
+    output_file = DATADIR / "test_output.txt"
+    result = runner.invoke(app, ["complexity", str(short_gfa_test_file), str(short_tsv_test_file), str(output_file)])
 
     assert result.exit_code == 0
     assert "Complexity" in result.output
