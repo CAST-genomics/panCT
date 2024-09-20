e6e4d312d338ac0288b5cb1dd3e15ed65d1bfb14
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2024-09-20 13:38:19 -0700

    copy over files from https://github.com/BrianPugh/python-template

--- panct/__init__.py
+++ panct/__init__.py
@@ -1,10 +1,11 @@
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    # handles py3.7, since importlib.metadata was introduced in py3.8
-    from importlib_metadata import version, PackageNotFoundError
+from importlib.metadata import version, PackageNotFoundError
 
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:
     __version__ = "unknown"
+
+__all__ = [
+   "Foo",
+]
+from pythontemplate._c_extension import Foo
