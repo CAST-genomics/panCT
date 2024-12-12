15bb5ba94347ee2f1890682a3b4fadb23288f348
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2024-12-12 23:23:18 +0000

    make script posix compliant

--- panct/build_node_sample_map.sh
+++ panct/build_node_sample_map.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env sh
 
-zcat "$1" | grep -E '^W' | cut -f 2,7 | sed 's/\t//;s/</\t/g;s/>/\t/g' | awk -F $'\t' -v 'OFS=\t' '{
+{ if [ "${1##*.}" = "gz" ]; then
+  gzip -dc "$1"
+else
+  cat "$1"
+fi } | grep -E '^W' | cut -f 2,7 | sed 's/	//;s/</	/g;s/>/	/g' | awk -F '	' -v OFS='	' '{
   for (i = 1; i <= NF; i++) {
     if (i == 1) {
       key = $i;
     } else {
-      print($i, key);
+      print $i, key;
     }
   }
-}' | sort -u -t $'\t' -k1,1n -k2,2 | awk -F $'\t' -v 'OFS=\t' '
+}' | sort -u -t '	' -k1,1n -k2,2 | awk -F '	' -v OFS='	' '
 current==$1 { line = line OFS $2; next; }
 { print line; current=$1; line=$0; }
-END { print line }' | awk -F $'\t' -v 'OFS=\t' 'NF {
+END { print line }' | awk -F '	' -v OFS='	' 'NF {
   print "" FS $1 FS $0
 }' > "$2"
