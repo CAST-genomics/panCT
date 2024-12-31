611924766b5c7a7096ef00aad1a71a6ef5211c9a
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2024-12-31 21:07:26 +0000

    add --version option to root panct cmd

--- panct/__main__.py
+++ panct/__main__.py
@@ -1,11 +1,60 @@
 #!/usr/bin/env python
 
+from enum import Enum
 from pathlib import Path
-import typer
 from typing_extensions import Annotated
+
+import typer
+
+from . import __version__
 from .complexity import AVAILABLE_METRICS
 
 app = typer.Typer()
+
+
+class Verbosity(str, Enum):
+    critical = "CRITICAL"
+    error = "ERROR"
+    waring = "WARNING"
+    info = "INFO"
+    debug = "DEBUG"
+    notset = "NOTSET"
+
+
+verbose = Annotated[
+    Verbosity,
+    typer.Option(
+        "-v",
+        "--verbosity",
+        case_sensitive=False,
+        help="The level of verbosity desired",
+    ),
+]
+
+
+def version_callback(value: bool = False):
+    if value:
+        typer.echo(__version__)
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: Annotated[
+        bool,
+        typer.Option(
+            "-v",
+            "--version",
+            callback=version_callback,
+            is_eager=True,
+            help="Show the application's version and exit.",
+        ),
+    ] = False,
+):
+    """
+    panct: A collection of tools for working with pangenomes
+    """
+    pass
 
 
 @app.command()
@@ -48,7 +97,7 @@ def complexity(
     output_file: Annotated[
         Path, typer.Option("--out", help="Name of output file")
     ] = Path("-"),
-    verbosity: str = "INFO",
+    verbosity: verbose = Verbosity.info,
 ):
     """
     Compute complexity scores
@@ -56,7 +105,7 @@ def complexity(
     from .complexity import main as complexity_main
     from .logging import getLogger
 
-    log = getLogger(name="complexity", level=verbosity)
+    log = getLogger(name="complexity", level=verbosity.value)
     retcode = complexity_main(
         graph, str(output_file), region, region_file, metrics, reference, log
     )
@@ -79,7 +128,7 @@ def walks(
     output_file: Annotated[
         Path, typer.Option("-o", "--out", help="Name of output file")
     ] = None,
-    verbosity: str = "INFO",
+    verbosity: verbose = Verbosity.info,
 ):
     """
     Extract walks to a file
@@ -87,12 +136,14 @@ def walks(
     from .walks import extract_walks
     from .logging import getLogger
 
-    log = getLogger(name="walks", level=verbosity)
+    log = getLogger(name="walks", level=verbosity.value)
 
     extract_walks(graph, output_file, log)
 
 
 typer_click_object = typer.main.get_command(app)
+
+
 if __name__ == "__main__":
     # Run the CLI when called from the command line or via python -m panct
     app(prog_name="panct")
